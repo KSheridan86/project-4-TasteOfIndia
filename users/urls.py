54715d2a6e3dd363fa87e37a3b48464821f4ca82
Ken Sheridan <kensheridan86@gmail.com>
2022-10-06 12:15:38 +0000

    login/logout functionality added

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('sign_in/', views.sign_in, name='sign_in'),
+    path('sign_out/', views.sign_out, name='sign_out'),
     path('', views.profiles, name='profiles'),
     path('user_profile/<str:pk>/', views.user_profile, name='user_profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 from .models import Profile
 
 
@@ -16,3 +18,27 @@ def user_profile(request, pk):
         'profile': profile
     }
     return render(request, 'users/user_profile.html', context)
+
+
+def sign_in(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except Exception:
+            print('username does not exist')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('recipes')
+        else:
+            print('username OR password is incorrect')
+    return render(request, 'users/sign_in.html')
+
+
+def sign_out(request):
+    logout(request)
+    return redirect('sign_in')
