d554ceac98968876971ce05dcac1a372fcf94bbb
Ken Sheridan <kensheridan86@gmail.com>
2022-10-08 18:24:44 +0000

    pagination & search form linked together with custom javascript

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,8 @@
 // Global Variables
 let closealert = document.querySelector('.close-btn')
 let messages = document.querySelector('.messages')
+let searchForm = document.querySelector('#searchForm');
+let pageBtn = document.querySelectorAll('.page-btn');
 
 // Code to close to message boxes automatically and allow you to close them manually
 if (closealert) {
@@ -10,4 +12,18 @@ if (closealert) {
     setTimeout(() => {
         messages.style.display = 'none'
     }, 3000);
+}
+
+// Code to link search form and pagination together
+if (searchForm) {
+    for (let i = 0; pageBtn.length > i; i++) {
+    pageBtn[i].addEventListener('click', function (event) {
+        event.preventDefault()
+        console.log('still here')
+        let page = this.dataset.page
+        console.log(page)
+        searchForm.innerHTML += `<input value=${page} name="page" hidden/>`
+        searchForm.submit()
+    })
+}
 }
