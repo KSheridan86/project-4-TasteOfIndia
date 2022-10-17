5a6afaead3e752da743575c378e330f37879bf37
Ken Sheridan <kensheridan86@gmail.com>
2022-10-17 14:33:46 +0000

    html validation complete, small changes made to pass tests

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,17 @@
 // Global Variables
-let closealert = document.querySelector('.close-btn')
-let messages = document.querySelector('.messages')
+let closealert = document.querySelector('.close-btn');
+let messages = document.querySelector('.messages');
 let searchForm = document.querySelector('#searchForm');
 let pageBtn = document.querySelectorAll('.page-btn');
 let topBtn = document.querySelector('.back-to-top');
 
 // Code to close the message boxes automatically and allow you to close them manually
 if (closealert) {
-    messages.style.display = 'block'
+    messages.style.display = 'block';
     closealert.addEventListener('click', () =>
-        messages.style.display = 'none')
+        messages.style.display = 'none');
     setTimeout(() => {
-        messages.style.display = 'none'
+        messages.style.display = 'none';
     }, 3000);
 }
 
@@ -19,11 +19,11 @@ if (closealert) {
 if (searchForm) {
     for (let i = 0; pageBtn.length > i; i++) {
     pageBtn[i].addEventListener('click', function (event) {
-        event.preventDefault()
-        let page = this.dataset.page
-        searchForm.innerHTML += `<input value=${page} name="page" hidden/>`
-        searchForm.submit()
-    })
+        event.preventDefault();
+        let page = this.dataset.page;
+        searchForm.innerHTML += `<input value=${page} name="page" hidden/>`;
+        searchForm.submit();
+    });
 }
 }
 
@@ -31,9 +31,9 @@ if (searchForm) {
 function backToTop() {
     if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
         topBtn.style.display = "block";
-      } else {
+        } else {
         topBtn.style.display = "none";
-      }
+        }
 }
 
-window.onscroll = function() {backToTop()};
+window.onscroll = function() {backToTop();};
